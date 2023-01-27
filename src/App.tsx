41f737f6d7675aaf5b9d71b99ba11be3efef6ebe
Wophe <wophe.akinlade@gmail.com>
2023-01-27 21:19:30 +0100

    Done with the main design and the mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import "./App.css";
 
 const App: React.FC = () => {
+  let advice: string =
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit.";
   return (
     <div className="App">
       <div className="advice-box">
         <h4 className="advice-box__advice-number">Advice #1</h4>
-        <h1 className="advice-box__advice">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        </h1>
-        <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+        <h1 className="advice-box__advice">"{advice}"</h1>
+        <svg
+          className="no-show--mobile divider"
+          width="444"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g fill="none" fill-rule="evenodd">
             <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
             <g transform="translate(212)" fill="#CEE3E9">
@@ -18,12 +23,42 @@ const App: React.FC = () => {
             </g>
           </g>
         </svg>
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
-            fill="#202733"
-          />
+        <svg className="no-show--desktop divider" width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+          <g fill="none" fill-rule="evenodd">
+            <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+            <g transform="translate(138)" fill="#CEE3E9">
+              <rect width="6" height="16" rx="3" />
+              <rect x="14" width="6" height="16" rx="3" />
+            </g>
+          </g>
         </svg>
+        <button className="advice-box__btn">
+          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+              fill="#202733"
+            />
+          </svg>
+        </button>
+      </div>
+      <div className="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://github.com/WopheAkinlade"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Wofe Akinlade
+        </a>
+        .
       </div>
     </div>
   );
